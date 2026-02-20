049ab113dd3b37aa7aa73ace770130279835b36b
divyanshu-exe <divyanshu892484@gmail.com>
2026-02-20 17:23:02 +0530

    add about me section

---
+++ script.js
@@ -0,0 +1,10 @@
+
+console.log(5+6)
+const downloadButton=document.querySelector("#cvButton");
+
+downloadButton.addEventListener("click", ()=>{
+     
+    const a=document.querySelector("#resumeTag");
+    
+    a.click();
+})
